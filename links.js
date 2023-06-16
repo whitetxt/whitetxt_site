e9251601a0e2551b3f7f01073275a1f79cd3c0e8
whitetxt <54776821+whitetxt@users.noreply.github.com>
2023-06-16 12:44:11 +0100

    Updates Updated styles - Added scroll to about, improved link icons Improved about Improved contact Added pronouns.page to links Improved cross-page animation.

--- links.js
+++ links.js
@@ -26,6 +26,8 @@ function randomize(elem, num = 1) {
   setTimeout(randomize, 40, elem, num);
 }
 var completed = [];
+var animation = null;
+var curTimeout = null;
 setInterval(() => {
   links = document.querySelectorAll("a");
   links.forEach((elem) => {
@@ -45,15 +47,33 @@ setInterval(() => {
         event.preventDefault();
         const href = elem.href;
         const trans = document.querySelector("div#transition");
-        trans.animate([{ top: "-100%" }, { top: "0%" }], {
+        trans.style.display = "block";
+        if (animation !== null) {
+          animation.finish();
+          clearTimeout(curTimeout);
+        }
+        animation = trans.animate([{ top: "-100%" }, { top: "0%" }], {
           duration: 750,
           iterations: 1,
           fill: "forwards",
           easing: "cubic-bezier(.22, .61, .36, 1)",
         });
         setTimeout(() => {
+          if (window.location.href === elem.href) {
+            const trans = document.querySelector("div#transition");
+            trans.animate([{ top: "0%" }, { top: "100%" }], {
+              duration: 750,
+              iterations: 1,
+              fill: "forwards",
+              easing: "cubic-bezier(.22, .61, .36, 1)",
+            });
+            setTimeout(() => {
+              trans.style.display = "none";
+            }, 750);
+            return;
+          }
           window.location.href = href;
-        }, 800);
+        }, 750);
       });
     }
     completed.push(elem.href);
@@ -61,10 +81,13 @@ setInterval(() => {
 }, 100);
 window.onload = () => {
   const trans = document.querySelector("div#transition");
-  trans.animate([{ top: "0%" }, { top: "100%" }], {
+  animation = trans.animate([{ top: "0%" }, { top: "100%" }], {
     duration: 750,
     iterations: 1,
     fill: "forwards",
     easing: "cubic-bezier(.22, .61, .36, 1)",
   });
+  curTimeout = setTimeout(() => {
+    trans.style.display = "none";
+  }, 750);
 };
