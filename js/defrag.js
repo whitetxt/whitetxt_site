6df524e8ba2ce676bc98e7147ae783f0c6c8985f
whitetxt <54776821+whitetxt@users.noreply.github.com>
2024-02-26 15:04:23 +0000

    Working on filesystem implementation

--- js/defrag.js
+++ js/defrag.js
@@ -1,7 +1,12 @@
 const template_defrag = document.querySelector("div.template.defrag");
 
 var defrag_windows = {};
-const directions = [[1, 0], [0, 1], [-1, 0], [0, -1]];
+const directions = [
+  [1, 0],
+  [0, 1],
+  [-1, 0],
+  [0, -1],
+];
 
 function openDefrag(file = null) {
   new_defrag = template_defrag.cloneNode(true);
@@ -18,21 +23,30 @@ function openDefrag(file = null) {
   head.addEventListener("mouseup", () => {
     selected_element = null;
   });
-  head.querySelector(".minimizebutton").addEventListener("click", function() {
-    this.dataset.mini = "yes";
-    this.style.opacity = "0";
-  }.bind(new_defrag));
-  head.querySelector(".maximizebutton").addEventListener("click", function() {
-    this.dataset.max = "yes";
-    this.style.width = "100%";
-    this.style.height = "calc(100% - 31px)";
-    this.style.top = "0";
-    this.style.left = "0";
-  }.bind(new_defrag));
-  head.querySelector(".quitbutton").addEventListener("click", function() {
-    this.parentElement.removeChild(this);
-    remove_taskbar_item(this.dataset.window_id);
-  }.bind(new_defrag));
+  head.querySelector(".minimizebutton").addEventListener(
+    "click",
+    function () {
+      this.dataset.mini = "yes";
+      this.style.opacity = "0";
+    }.bind(new_defrag)
+  );
+  head.querySelector(".maximizebutton").addEventListener(
+    "click",
+    function () {
+      this.dataset.max = "yes";
+      this.style.width = "100%";
+      this.style.height = "calc(100% - 31px)";
+      this.style.top = "0";
+      this.style.left = "0";
+    }.bind(new_defrag)
+  );
+  head.querySelector(".quitbutton").addEventListener(
+    "click",
+    function () {
+      this.parentElement.removeChild(this);
+      remove_taskbar_item(this.dataset.window_id);
+    }.bind(new_defrag)
+  );
 
   // Generate squares
   const square_div = new_defrag.querySelector("div.squares");
@@ -46,25 +60,40 @@ function openDefrag(file = null) {
     }
   }
 
-  new_defrag.addEventListener("click", function() {
-    focus_window(this.dataset.window_id);
-  }.bind(new_defrag));
+  new_defrag.addEventListener(
+    "click",
+    function () {
+      focus_window(this.dataset.window_id);
+    }.bind(new_defrag)
+  );
 
-  new_defrag.querySelector("div.start").addEventListener("click", function() {
-    start_defrag(this.dataset.window_id);
-  }.bind(new_defrag));
+  new_defrag.querySelector("div.start").addEventListener(
+    "click",
+    function () {
+      start_defrag(this.dataset.window_id);
+    }.bind(new_defrag)
+  );
 
-  new_defrag.querySelector("div.quit").addEventListener("click", function() {
-    stop_defrag(this.dataset.window_id);
-  }.bind(new_defrag));
-  
+  new_defrag.querySelector("div.quit").addEventListener(
+    "click",
+    function () {
+      stop_defrag(this.dataset.window_id);
+      this.parentElement.removeChild(this);
+      remove_taskbar_item(this.dataset.window_id);
+    }.bind(new_defrag)
+  );
+
   windows_open[++last_window_id] = new_defrag;
-  windows_open[last_window_id].keydown = function(e) {
+  windows_open[last_window_id].keydown = function (e) {
     defrag_keypress(this.dataset.window_id, e);
   }.bind(new_defrag);
   new_defrag.dataset.window_id = last_window_id;
   new_defrag.id = last_window_id;
-  create_taskbar_item("Disk Defragmenter", "assets/appicons/defrag.png", last_window_id);
+  create_taskbar_item(
+    "Disk Defragmenter",
+    "assets/appicons/defrag.png",
+    last_window_id
+  );
   focus_window(last_window_id);
   desktop.appendChild(new_defrag);
 }
@@ -73,10 +102,25 @@ function start_defrag(window_id) {
   stop_defrag(window_id);
   setTimeout(() => {
     windows_open[window_id].querySelector("span.defrag-prog").innerText = `0%`;
-    windows_open[window_id].querySelector("span.data").style.display = "inline-block";
-    windows_open[window_id].querySelector("span.defrag-prog").style.display = "inline-block";
+    windows_open[window_id].querySelector("span.data").style.display =
+      "inline-block";
+    windows_open[window_id].querySelector("span.defrag-prog").style.display =
+      "inline-block";
     windows_open[window_id].querySelector("span.status").innerHTML = "";
-    defrag_windows[window_id] = {"completed": [], "length": 5, "snake": [[0, 0], [0, 0], [1, 0], [2, 0], [3, 0]], "direction": 0, "quit": false, "fruit": [20, 10]};
+    defrag_windows[window_id] = {
+      completed: [],
+      length: 5,
+      snake: [
+        [0, 0],
+        [0, 0],
+        [1, 0],
+        [2, 0],
+        [3, 0],
+      ],
+      direction: 0,
+      quit: false,
+      fruit: [20, 10],
+    };
     const squares = windows_open[window_id].querySelectorAll("div.square");
     for (var i = 0; i < squares.length; i++) {
       squares[i].classList.remove("snake");
@@ -85,7 +129,10 @@ function start_defrag(window_id) {
         squares[i].classList.add("fruit");
       } else {
         squares[i].classList.add("completed");
-        defrag_windows[window_id]["completed"].push([i % 40, Math.floor(i / 40)]);
+        defrag_windows[window_id]["completed"].push([
+          i % 40,
+          Math.floor(i / 40),
+        ]);
       }
     }
     setTimeout(perform_defrag, 50, window_id);
@@ -106,7 +153,10 @@ function perform_defrag(window_id) {
   }
   defrag_windows[window_id]["snake"].shift();
   const snake = defrag_windows[window_id]["snake"];
-  var head = defrag_windows[window_id]["snake"][defrag_windows[window_id]["snake"].length - 1];
+  var head =
+    defrag_windows[window_id]["snake"][
+      defrag_windows[window_id]["snake"].length - 1
+    ];
   var new_head = [...head];
   new_head[0] += directions[defrag_windows[window_id]["direction"]][0];
   new_head[1] += directions[defrag_windows[window_id]["direction"]][1];
@@ -118,7 +168,7 @@ function perform_defrag(window_id) {
   if (new_head[1] < 0) {
     new_head[1] += 20;
   }
-  if (snake.some(a => new_head.every((v, i) => v === a[i]))) {
+  if (snake.some((a) => new_head.every((v, i) => v === a[i]))) {
     defrag_windows[window_id]["snake"].push(new_head);
     for (var i = 0; i < defrag_windows[window_id]["snake"].length; i++) {
       const snake = defrag_windows[window_id]["snake"][i];
@@ -126,15 +176,19 @@ function perform_defrag(window_id) {
       squares[snake[0] + snake[1] * 40].classList.add("dead");
     }
     windows_open[window_id].querySelector("span.data").style.display = "none";
-    windows_open[window_id].querySelector("span.defrag-prog").style.display = "none";
-    windows_open[window_id].querySelector("span.status").classList.remove("win");
+    windows_open[window_id].querySelector("span.defrag-prog").style.display =
+      "none";
+    windows_open[window_id]
+      .querySelector("span.status")
+      .classList.remove("win");
     windows_open[window_id].querySelector("span.status").classList.add("lose");
-    windows_open[window_id].querySelector("span.status").innerHTML = "Defragmentation Failed:<br>Hard Drive Disk Platters Collapsed.";
+    windows_open[window_id].querySelector("span.status").innerHTML =
+      "Defragmentation Failed:<br>Hard Drive Disk Platters Collapsed.";
     return;
   }
   defrag_windows[window_id]["snake"].push(new_head);
   const fruit = defrag_windows[window_id]["fruit"];
-  if (snake.some(a => fruit.every((v, i) => v === a[i]))) {
+  if (snake.some((a) => fruit.every((v, i) => v === a[i]))) {
     const current_index = fruit[0] + fruit[1] * 40;
     squares[current_index].classList.add("completed");
     squares[current_index].classList.remove("fruit");
@@ -151,26 +205,36 @@ function perform_defrag(window_id) {
     }
     if (indexes.length === 0) {
       const num_comp = defrag_windows[window_id]["completed"].length;
-      windows_open[window_id].querySelector("span.defrag-prog").innerText = `${Math.floor((num_comp * 100) / (40 * 20))}%`;
+      windows_open[window_id].querySelector(
+        "span.defrag-prog"
+      ).innerText = `${Math.floor((num_comp * 100) / (40 * 20))}%`;
       for (var i = 0; i < squares.length; i++) {
         squares[i].classList.add("win");
         squares[i].classList.remove("dead");
         squares[i].classList.remove("snake");
         squares[i].classList.remove("completed");
-        squares[i].style.animationDelay = `-${Math.round(Math.random() * 50) * 10}ms`;
+        squares[i].style.animationDelay = `-${
+          Math.round(Math.random() * 50) * 10
+        }ms`;
       }
       windows_open[window_id].querySelector("span.data").style.display = "none";
-      windows_open[window_id].querySelector("span.defrag-prog").style.display = "none";
-      windows_open[window_id].querySelector("span.status").innerHTML = "Defragmentation has been completed!";
+      windows_open[window_id].querySelector("span.defrag-prog").style.display =
+        "none";
+      windows_open[window_id].querySelector("span.status").innerHTML =
+        "Defragmentation has been completed!";
       windows_open[window_id].querySelector("span.status").classList.add("win");
-      windows_open[window_id].querySelector("span.status").classList.remove("lose");
+      windows_open[window_id]
+        .querySelector("span.status")
+        .classList.remove("lose");
       return;
     } else {
       const index = indexes[Math.floor(Math.random() * indexes.length)];
       defrag_windows[window_id]["fruit"][1] = Math.floor(index / 40);
       defrag_windows[window_id]["fruit"][0] = index % 40;
       squares[index].classList.add("fruit");
-      defrag_windows[window_id]["snake"].unshift([...defrag_windows[window_id]["snake"][0]]);
+      defrag_windows[window_id]["snake"].unshift([
+        ...defrag_windows[window_id]["snake"][0],
+      ]);
       console.log(defrag_windows[window_id]["fruit"]);
     }
   }
@@ -179,7 +243,9 @@ function perform_defrag(window_id) {
     squares[snake[0] + snake[1] * 40].classList.add("snake");
   }
   const num_comp = defrag_windows[window_id]["completed"].length;
-  windows_open[window_id].querySelector("span.defrag-prog").innerText = `${Math.floor((num_comp * 100) / (40 * 20))}%`;
+  windows_open[window_id].querySelector(
+    "span.defrag-prog"
+  ).innerText = `${Math.floor((num_comp * 100) / (40 * 20))}%`;
   setTimeout(perform_defrag, 50, window_id);
 }
 
---
+++ js/fs.js
@@ -0,0 +1,92 @@
+class FakeFileSystem {
+  constructor() {
+    if (!localStorage.getItem("wtxt-fs")) {
+      localStorage.setItem("wtxt-fs", JSON.stringify({}));
+    }
+  }
+
+  getFS() {
+    return JSON.parse(localStorage.getItem("wtxt-fs"));
+  }
+
+  getFSDir(path) {
+    var fs = this.getFS();
+    var cur_path = path;
+    while (path.length !== 0) {
+      cur_path = path[0];
+      if (Object.keys(cur_fs).indexOf(cur_path) === -1) {
+        throw new Error("Path does not exist.");
+      }
+      cur_fs = cur_fs[cur_path];
+      path.shift();
+    }
+    return fs;
+  }
+
+  saveFS(filesystem) {
+    localStorage.setItem("wtxt-fs", JSON.stringify(filesystem));
+  }
+
+  parsePath(path) {
+    if (typeof path !== "string") {
+      throw new Error("Invalid parameter to parsePath");
+    }
+    if (path[0] !== "/") {
+      throw new Error("Invalid path");
+    }
+    path = path.split("/");
+    path.shift();
+    return path;
+  }
+
+  readFile(filePath) {
+    filePath = this.parsePath(filePath);
+    const filename = filePath.pop();
+    const filesystem = this.getFSDir(filePath);
+    if (
+      filesystem[filename] !== undefined &&
+      typeof filesystem[filename] == "object"
+    ) {
+      throw new Error("Cannot read a directory.");
+    }
+    return filesystem[filename] || null;
+  }
+
+  writeFile(filePath, data) {
+    filePath = this.parsePath(filePath);
+    const filename = filePath.pop();
+    const filesystem = this.getFSDir(filePath);
+    if (
+      filesystem[filename] !== undefined &&
+      typeof filesystem[filename] == "object"
+    ) {
+      throw new Error("Cannot write to a directory.");
+    }
+    filesystem[filename] = data;
+    this.saveFS(filesystem);
+  }
+
+  deleteFile(filePath) {
+    const filesystem = JSON.parse(localStorage.getItem("filesystem"));
+    delete filesystem[filePath];
+    this.saveFS(filesystem);
+  }
+
+  createDir(path) {
+    path = this.parsePath(path);
+    var fs = this.getFS();
+    var cur_path;
+    var cur_fs = fs;
+    while (path.length !== 0) {
+      cur_path = path[0];
+      if (Object.keys(cur_fs).indexOf(cur_path) === -1) {
+        cur_fs[cur_path] = {};
+      }
+      cur_fs = cur_fs[cur_path];
+      path.shift();
+    }
+    this.saveFS(fs);
+  }
+}
+
+const fs = new FakeFileSystem();
