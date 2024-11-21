00bde81091d0084b2ad312e7345a2974a822a98f
_whitetxt <whitetxt@whitetxt.dev>
2024-11-21 20:39:40 +0000

    Added check to see if main host is online

--- static/script/backup.js
+++ static/script/backup.js
@@ -1,6 +1,20 @@
-if (window.location.host === "backup.whitetxt.dev") {
+const elem = document.createElement("div");
+elem.id = "hostCheck";
+elem.classList.add("absolute", "left-0", "top-0", "flex", "flex-row", "items-center", "p-4", "gap-4", "hidden");
+elem.innerHTML = `<span class="loading loading-ring loading-sm" id="hostCheckLoading"></span>
+<span id="hostCheckText">Checking if host is online...</span>`;
+document.body.appendChild(elem);
+
+const hostCheck = document.querySelector("#hostCheck");
+const hostCheckLoading = document.querySelector("#hostCheckLoading");
+const hostCheckText = document.querySelector("#hostCheckText");
+
+if (window.location.host === "backup.whitetxt.dev" || true) {
     const clips = document.querySelector("a[href='clips.php']");
     clips.href = "/clips.html";
+    hostCheck.classList.remove("hidden");
+    checkHost();
+    setInterval(checkHost, 60 * 1000);
     if (window.location.search.includes("?d=1")) {
         const hero = document.createElement("div");
         hero.id = "backuphero";
@@ -13,7 +27,7 @@ if (window.location.host === "backup.whitetxt.dev") {
         Sorry! My host is down at the moment, so you've been redirected to a backup!
       </p>
       <p class="pb-6">
-        Check if whitetxt.dev is back up by visiting <a href="https://whitetxt.dev">clicking here</a>.
+        Check if whitetxt.dev is back up by visiting <a href="https://whitetxt.dev" class="text-primary">clicking here</a>.
       </p>
       <button class="btn btn-primary" onclick="closeBackupHero()">Close</button>
     </div>
@@ -24,4 +38,33 @@ if (window.location.host === "backup.whitetxt.dev") {
 
 function closeBackupHero() {
     document.querySelector("#backuphero").remove();
+}
+
+function checkHost() {
+    hostCheckLoading.classList.remove("hidden");
+    hostCheckText.innerText = "Checking if host is online...";
+    fetch("https://whitetxt.dev/online_check.html").then((res) => {
+        if (res.status !== 200) {
+            // Host offline
+            hostCheckLoading.classList.add("hidden");
+            hostCheckText.innerText = "Host offline. Rechecking in 1 minute.";
+            return;
+        }
+        // Host online
+        hostCheckLoading.classList.add("hidden");
+        hostCheckText.innerText = "Host is back online! Please go to whitetxt.dev";
+        const hero = document.createElement("div");
+        hero.id = "backuphero";
+        hero.classList.add("hero", "bg-base-200", "min-h-[50vh]", "absolute", "left-1/2", "top-1/2", "transform", "-translate-x-1/2", "-translate-y-1/2", "w-1/4");
+        hero.innerHTML = `
+  <div class="hero-content text-center">
+    <div class="max-w-md">
+      <h1 class="text-5xl font-bold">Hooray!</h1>
+      <p class="pt-6 text-2xl">
+        My host is back online! Please head over there: <a href="https://whitetxt.dev" class="text-primary">whitetxt.dev</a>
+      </p>
+    </div>
+  </div>`;
+        document.body.appendChild(hero);
+    });
 }
