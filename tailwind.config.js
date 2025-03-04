53952c7984c237abe2ecfb64d0f646bfc9ef2857
_whitetxt <whitetxt@whitetxt.dev>
2025-03-04 23:33:53 +0000

    [TEMP] Remove DaisyUI so tailwind can upgrade

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./*.html", "./**/*.html", "./**/*.php", "./static/script/*.js"],
-    theme: {
-        extend: {},
-    },
-    plugins: [require("daisyui")],
-    daisyui: {
-        themes: ["luxury"],
-    },
+  content: ["./*.html", "./**/*.html", "./**/*.php", "./static/script/*.js"],
+  theme: {
+    extend: {},
+  },
 };
